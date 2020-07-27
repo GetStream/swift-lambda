5c8f17ce95c94c213f733461a24947e8fbae9f3e
cardoso <matheus@cardo.so>
2020-07-27 10:18:30 -0300

    Add docker builder image step

--- Scripts/build-and-package.sh
+++ Scripts/build-and-package.sh
@@ -2,6 +2,10 @@ set -eu
 
 executable="Lambda"
 
+echo "preparing docker build image"
+docker build . -t builder
+echo "done"
+
 echo "building executable"
 docker run --rm -v "$(pwd)":/workspace -w /workspace builder bash -cl "swift build --product $executable -c release -Xswiftc -g"
 echo "done"
